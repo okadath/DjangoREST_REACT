ef89254a22fedea72b4b2e3231a678937dcfc69f
okadath <vintaw.01@gmail.com>
2020-01-25 17:22:04 -0600

    permissions

--- backend/todo_project/todo_project/settings.py
+++ backend/todo_project/todo_project/settings.py
@@ -53,9 +53,9 @@ SWAGGER_SETTINGS = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
